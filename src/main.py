b6f73615d95906b94e9d97b58d1b673ac3b66ea5
Siddique Data Engineer <dba.siddique@gmail.com>
2022-11-08 11:45:00 +0500

    Implement data processing pipeline

---
+++ src/main.py
@@ -0,0 +1,16 @@
+"""
+104 Api Data Ingestion - Main Implementation
+"""
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def main():
+    logger.info("Starting 104-api-data-ingestion")
+    # Implementation here
+    pass
+
+if __name__ == "__main__":
+    main()
